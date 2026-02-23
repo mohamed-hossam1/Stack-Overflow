d374a9943717f07e7bf952a40f6d0e8f4131a1c6
Mohamed Hossam <mohamedhossamv8@gmail.com>
2026-02-23 10:02:10 +0200

    feat: add UI for QuestionForm component

---
+++ app/(root)/ask-question/page.tsx
@@ -0,0 +1,17 @@
+import QuestionForm from "@/components/forms/QuestionForm";
+import React from "react";
+
+
+const AskAQuestion = () => {
+  return (
+    <>
+      <h1 className="h1-bold text-dark100_light900">Ask a question</h1>
+
+      <div className="mt-9">
+        <QuestionForm />
+      </div>
+    </>
+  );
+};
+
+export default AskAQuestion;
