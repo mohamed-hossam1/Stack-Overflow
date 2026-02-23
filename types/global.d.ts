69cab344234affa2091b0399330212030f85a822
Mohamed Hossam <mohamedhossamv8@gmail.com>
2026-02-23 09:31:12 +0200

    feat: enhance home page with filtering options

---
+++ types/global.d.ts
@@ -0,0 +1,23 @@
+declare module "*.css"
+
+interface Tag {
+  _id: string;
+  name: string;
+}
+
+interface Author {
+  _id: string;
+  name: string;
+  image: string;
+}
+
+interface Question {
+  _id: string;
+  title: string;
+  tags: Tag[];
+  author: Author;
+  upvotes: number;
+  answers: number;
+  views: number;
+  createdAt: Date;
+}
